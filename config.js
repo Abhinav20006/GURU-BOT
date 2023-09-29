73b547d567cf9dbf344b07037214fd63f65c1570
Abhinav20006 <143079865+Abhinav20006@users.noreply.github.com>
2023-09-30 00:11:32 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,16 +7,16 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', '𝔄𝔟𝔥𝔦𝔫𝔞𝔳', true],
-  ['6281255369012', '𝐍𝐄𝐙𝐔𝐊𝐎', true], 
+  ['12034902941', '𝔄𝔟𝔥𝔦𝔫𝔞𝔳', true],
+  ['16367643392', '𝐍𝐄𝐙𝐔𝐊𝐎', true], 
   [''] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
-global.mods = ['917605902011','32460248586'] 
-global.prems = ['917605902011', '32460248586', '919398758484']
-global.allowed = ['917605902011', '32460248586', '19152999993']
+global.mods = ['12034902941','16367643392'] 
+global.prems = ['12034902941', '16367643392']
+global.allowed = ['12034902941', '16367643392']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
